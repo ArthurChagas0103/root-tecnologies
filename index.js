c63d9341095251288c34714bf29c2fb0aef74fb9
ArthurChagas0103 <arthurgchagas@gmail.com>
2022-11-05 18:56:24 -0300

    05/11/2022

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function mudarCor() {
 	}
 }
 window.addEventListener('scroll', mudarCor)
+
+let ativado = 0;
+
+function clickMudarCor() {
+	if (window.scrollY < 1) {
+		ativado++;
+
+		if (ativado % 2 == 0) {
+			document.getElementById('navbar').style.backgroundColor = 'rgba(0, 0, 0, 0)'
+		}
+		else {
+			document.getElementById('navbar').style.backgroundColor = '#1B2029'
+		}
+	}
+	else{
+		document.getElementById('navbar').style.backgroundColor = '#1B2029'
+	}
+}
