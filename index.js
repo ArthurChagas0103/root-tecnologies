33065945c30ba9da6e379c7aade49642a17647c0
LucasMeloSena <lucasmelosena@gmail.com>
2022-11-08 17:12:55 -0300

    08/11/2022

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ function clickMudarCor() {
 		document.getElementById('navbar').style.backgroundColor = '#1B2029';
 	}
 }
+
+function preenchimento() 
+{
+	document.getElementById("nome").placeholder = "John Doe";
+	document.getElementById("celular").placeholder = "+55 (XX) 9 XXXX-XXXX";
+	document.getElementById("email").placeholder = "example@email.com";
+	document.getElementById("mensagem").placeholder = "Digite aqui sua mensagem..."
+}
+
+window.addEventListener("load", preenchimento)
